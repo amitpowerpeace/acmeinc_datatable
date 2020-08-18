0dafaa7d8997b4653cd1227bff87997adb4fbe8f
Amit <akp.332@gmail.com>
2020-08-18 12:02:00 +0530

    acme datatable test

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductListContainer from './container/ProductListContainer'
+
+const App = props => {
+  return (
+    <div className="container">
+      <ProductListContainer/>
+    </div>
+  );
+};
+
+export default App;

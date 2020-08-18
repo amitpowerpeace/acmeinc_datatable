8674f5ed6efce8bed98f7ee5faa663ba1865ea85
Amit <akp.332@gmail.com>
2020-08-18 12:07:01 +0530

    Updates

---
+++ precache-manifest.d69251add6e2626102be91ba127eb403.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8138f796931922ab826b13bc28986740",
+    "url": "/acmeinc_datatable/index.html"
+  },
+  {
+    "revision": "ab762e57265701c180dd",
+    "url": "/acmeinc_datatable/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "40f480adc6f736344578",
+    "url": "/acmeinc_datatable/static/css/main.d656fa81.chunk.css"
+  },
+  {
+    "revision": "ab762e57265701c180dd",
+    "url": "/acmeinc_datatable/static/js/2.2847272c.chunk.js"
+  },
+  {
+    "revision": "40f480adc6f736344578",
+    "url": "/acmeinc_datatable/static/js/main.5986568f.chunk.js"
+  },
+  {
+    "revision": "f8d1e1d0d70166f93281",
+    "url": "/acmeinc_datatable/static/js/runtime~main.c2625861.js"
+  }
+]);
